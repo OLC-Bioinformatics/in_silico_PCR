d3eb8f3834079b508c4e3530fea531bc7ebbdcfa
Adam Koziol <adam.koziol@canada.ca>
2022-07-11 13:33:44 -0400

    Changed version tracking method

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+from distutils.util import convert_path
 import os
+__author__ = 'adamkoziol'
+
+# Find the version
+version = dict()
+with open(convert_path(os.path.join('primer_finder', 'version.py')), 'r') as version_file:
+    exec(version_file.read(), version)
+
 setup(
     name="in_silico_pcr",
-    version="0.0.2.4",
-    scripts=[os.path.join('primer_finder', 'primer_finder.py')],
+    version=version['__version__'],
+    scripts=[
+        os.path.join('primer_finder', 'primer_finder.py'),
+        os.path.join('primer_finder', 'primer_validator.py')
+    ],
     packages=find_packages(),
     include_package_data=True,
     author="Adam Koziol",
     author_email="adam.koziol@inspection.gc.ca",
-    url="https://github.com/adamkoziol/in_silico_PCR",
+    url="https://github.com/OLC-Bioinformatics/in_silico_PCR",
 )
